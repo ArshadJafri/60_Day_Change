9dbc9e59b0730df65b08a25cbfc8a1007820f949
Arshad Jafri <45098927+ArshadJafri@users.noreply.github.com>
2025-06-27 20:37:32 -0500

    Create hr4.py

---
+++ hr4.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    count = {}
+    
+    for i in a:
+        count[i] = count.get(i,0)+1
+    for num, key in count.items():
+        if key == 1:
+            return num
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
